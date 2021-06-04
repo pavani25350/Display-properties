43bc746779dbae5463c21cafdc9dcb01fc27f0c5
pavani.uppala <79527235+pavani25350@users.noreply.github.com>
2021-06-04 12:25:06 +0530

    Add files via upload

---
+++ disappear.js
@@ -0,0 +1,12 @@
+function myfunction(){
+     document.getElementById("demo").style.display="none";
+}
+function myfunction1()
+{
+    document.getElementById("demo1").style.visibility="hidden";
+}
+function myfunction2()
+{
+    document.getElementById("demo").style.display="block";
+    document.getElementById("demo1").style.visibility="visible";
+}
